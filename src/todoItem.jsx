56fb94c0297458fff855da18827c2b8ecc0f3fc5
msheezi <marshall.randolph@gmail.com>
2019-12-25 14:57:55 -0800

    base

--- src/todoItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
- const TodoItem = ({myLists}) => myLists.map(item => <div style={{textAlign:"center", }}>{item}</div>)
- 
-
-
-export default TodoItem
--- src/todos.jsx
+++ src/todos.jsx
@@ -16,60 +16,6 @@ const initialState = {
     George: ["Make more lists", "Check things off list"]
 }
 
-class Todos extends React.Component{
-    constructor(props){
-        super(props)
 
-        this.state =  initialState
-                 
-    }
-
-    getTodos(name) {
-        let values = prompt("enter Todo") // get a due date, check to complete
-        let newTodos = this.state[name]
-        console.log(newTodos)
-        newTodos.push(values)
-        this.setState({[name]: newTodos}) 
-       
-    }
-    
-   
-
-    render(){
-        let todos = Object.keys(this.state).map(name => {
-            return (
-                <div style={{ marginRight: "25px", width: "25%", backgroundColor: "lightgray", border: "1px solid black"}} >
-                    <div style={{ backgroundColor: `${colors[name]}`, color: "white", textAlign: "center", borderRadius: "3px", position: "relative" }}>{name}</div>
-                        <TodoItem 
-                            myLists={this.state[name]}/>
-                            <button style={{width: "100%", 
-                            backgroundColor: "white", 
-                            alignSelf: "end",
-                            // color: "white"
-                            borderRadius: "3px",
-                        }} 
-                            onClick={()=> this.getTodos(name)}
-                            
-                            >Add Todo
-                            </button>
-                </div>
-            )
-        })
-              
-        return (
-
-            <div style={{
-                display: "flex", 
-                flexDirection:"row", 
-                width: "1000px",
-                
-                textAlign:"center" }}
-                >{todos}
-            </div>
-        )
-
-    }
-
-}
 
 export default Todos
